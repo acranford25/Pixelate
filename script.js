21c2b914478897e52cd49d4ca860a8426492938e
Andrew Cranford <acranford25@gmail.com>
2023-03-06 10:56:46 -0500

    worked through the steps

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-// Your code here
+// You code here
+const table = document.querySelector("#table")
+const button = document.querySelector("#add-row")
+const select = document.querySelector("#select")
+
+function makeRow () {
+    const row= document.createElement('tr')
+    for (let i =0; i < 20; i++) {
+        const td = document.createElement('td')
+        row.appendChild(td)
+    }
+    table.appendChild(row)
+}
+
+
+function colorize(event) {
+    const target = event.target
+    if (target.className === color) {
+        target.className = ''
+    } else {
+        target.className = color
+    }
+}
+
+let color = 'red'
+
+select.addEventListener('change', function(event){
+    color = event.target.value
+})
+table.addEventListener('click', colorize)
+button.addEventListener('click', makeRow)
