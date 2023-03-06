2bba7b14d1dc9106f40f2db3cd07953fcd8f5bf6
Vanessaines2 <Vanessaines2@icloud.com>
2023-03-06 11:31:09 -0500

    first extra credit

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 // You code here
-const table = document.querySelector("#table")
-const button = document.querySelector("#add-row")
-const select = document.querySelector("#select")
+const table = document.querySelector("#table");
+const button = document.querySelector("#add-row");
+const select = document.querySelector("#select");
 
-function makeRow () {
-    const row= document.createElement('tr')
-    for (let i =0; i < 20; i++) {
-        const td = document.createElement('td')
-        row.appendChild(td)
-    }
-    table.appendChild(row)
+function makeRow() {
+  const row = document.createElement("tr");
+  for (let i = 0; i < 20; i++) {
+    const td = document.createElement("td");
+    row.appendChild(td);
+  }
+  table.appendChild(row);
 }
-
 
 function colorize(event) {
-    const target = event.target
+  const target = event.target;
+  if (event.target.tagName === "TD") {
     if (target.className === color) {
-        target.className = ''
+      target.className = "";
     } else {
-        target.className = color
+      target.className = color;
     }
+  }
 }
 
-let color = 'red'
+let color = "red";
 
-select.addEventListener('change', function(event){
-    color = event.target.value
-})
-table.addEventListener('click', colorize)
-button.addEventListener('click', makeRow)
+select.addEventListener("change", function (event) {
+  color = event.target.value;
+});
+table.addEventListener("click", colorize);
+button.addEventListener("click", makeRow);
